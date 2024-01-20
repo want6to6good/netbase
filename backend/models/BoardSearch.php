3d1c7b03f598b54ece91ea7c4b8cb3523d4e8917
Miyilin2003 <1206164479@qq.com>
2024-01-21 02:39:24 +0800

    Update BoardSearch.php

--- backend/models/BoardSearch.php
+++ backend/models/BoardSearch.php
@@ -41,11 +41,11 @@ class BoardSearch extends Board
     /**
      * 这是类的核心功能，用于构建基于用户输入的搜索查询。
      * 首先，创建一个基于 Board 模型的查询 (Board::find())。
-     *接着，创建一个 ActiveDataProvider 实例，它将用于管理数据的获取、排序和分页。
-     *load($params) 用于加载搜索表单提交的数据。
-     *validate() 确保提供的数据符合 rules 方法定义的规则。
-     *如果验证失败，默认行为是返回所有数据。可以取消注释 $query->where('0=1'); 来在验证失败时返回空数据集。
-     *andFilterWhere 方法用于添加条件到查询中。这里根据 id、date 和 content 字段的值添加筛选条件。
+     * 接着，创建一个 ActiveDataProvider 实例，它将用于管理数据的获取、排序和分页。
+     * load($params) 用于加载搜索表单提交的数据。
+     * validate() 确保提供的数据符合 rules 方法定义的规则。
+     * 如果验证失败，默认行为是返回所有数据。可以取消注释 $query->where('0=1'); 来在验证失败时返回空数据集。
+     * andFilterWhere 方法用于添加条件到查询中。这里根据 id、date 和 content 字段的值添加筛选条件。
      */
     public function search($params)
     {
