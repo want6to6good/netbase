8c42f2c688fb04d39876abb4cc17ed0ab47c275a
Miyilin2003 <1206164479@qq.com>
2024-01-21 02:41:51 +0800

    Update ContectSearch.php

--- backend/models/ContectSearch.php
+++ backend/models/ContectSearch.php
@@ -38,6 +38,14 @@ class ContectSearch extends Contect
      *
      * @return ActiveDataProvider
      */
+    /**
+     * 首先，创建一个基于 Contect 模型的查询 (Contect::find())。
+     * 接着，创建一个 ActiveDataProvider 实例，它将用于管理数据的获取、排序和分页。
+     * load($params) 用于加载搜索表单提交的数据。
+     * validate() 确保提供的数据符合 rules 方法定义的规则。
+     * 如果验证失败，默认行为是返回所有数据。可以取消注释 $query->where('0=1'); 来在验证失败时返回空数据集。
+     * andFilterWhere 方法用于添加条件到查询中。根据 id、name、mail 和 content 字段的值添加筛选条件。
+     */
     public function search($params)
     {
         $query = Contect::find();
